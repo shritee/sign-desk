74080beb6991061c06f5d129aa778f326a3f1f3b
shritee <sarwakinshritee@gmail.com>
2021-06-27 14:55:54 +0530

    sign desk website

---
+++ signdisk.js
@@ -0,0 +1,18 @@
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
+
+// function m1(){
+//     var exampleFormControlInput1 = document.getElementById('exampleFormControlInput1')
+//     exampleFormControlInput1.style = ""
+// }
+function m1()
+
+{
+    var un=document.getElementById("exampleFormControlInput1");
+    un.style.color="blue"
+
+    // un.style.visibility="visible"
+    
+}
